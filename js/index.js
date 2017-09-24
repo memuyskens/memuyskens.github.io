e4d117de71cf5ce72cfcaf66b1cf3de413c1f44e
Morgan Muyskens <morganmuyskens@gmail.com>
2017-09-24 02:22:46 -0400

    Update UI and add education, experience, and activities information.

---
+++ js/index.js
@@ -0,0 +1,64 @@
+var site;
+
+$(document).ready(function() {
+    site = new Site();
+    resize();
+});
+
+$(window).resize(function () {
+  resize();
+});
+
+function resize() {
+  if ($(window).width() <= 880 && !$("body").hasClass("mobile")) {
+    $("body").addClass("mobile");
+    site.present("resume");
+  } else if ($(window).width() > 880 && $("body").hasClass("mobile")) {
+    $("body").removeClass("mobile");
+  }
+}
+
+function Site() {
+    var that = this;
+
+    $("#link-home").click(function (e) {
+        e.preventDefault();
+        if (!$("body").hasClass("mobile")) {
+          that.present("home");
+        }
+    });
+
+    $("#link-education").click(function (e) {
+        e.preventDefault();
+        that.present("education");
+    });
+
+    $("#link-experience").click(function (e) {
+        e.preventDefault();
+        that.present("experience");
+    });
+
+    $("#link-activities").click(function (e) {
+        e.preventDefault();
+        that.present("activities");
+    });
+
+    $("#link-projects").click(function (e) {
+        e.preventDefault();
+        that.present("projects");
+    });
+
+    $("#link-resume").click(function (e) {
+        e.preventDefault();
+        that.present("resume");
+    });
+
+    this.present("home");
+}
+
+Site.prototype.present = function (page) {
+  $("header > div > nav > a").removeClass("active");
+  $("#link-" + page).addClass("active");
+  $("main > div").hide();
+  $("#" + page).show();
+};
